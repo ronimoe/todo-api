73ed51a5c4b5b2b875edac9f32c6bbd321cf990a
Roni <roni@Ronis-MacBook-Pro.local>
2018-05-28 11:56:44 +0700

    Adding POST /todos

--- server/server.js
+++ server/server.js
@@ -23,6 +23,14 @@ app.post('/todos', (req, res) => {
   });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+})
+
 app.listen(7000, () => {
   console.log('Server started on port 7000!')
 });
